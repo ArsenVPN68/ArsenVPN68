ad843290cdde5f4dbee6dcc34d6307135de6db7c
ArsenVPN68 <152499092+ArsenVPN68@users.noreply.github.com>
2025-04-30 12:51:47 +0330

    install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 apt update -y
-curl -o $PREFIX/bin/68 https://raw.githubusercontent.com/Arsenlopez09/ArsenVPN68/refs/heads/main/Sel.sh
+curl -o $PREFIX/bin/68 https://raw.githubusercontent.com/ArsenVPN68/ArsenVPN68/refs/heads/main/Sel.sh
 chmod +x $PREFIX/bin/68
